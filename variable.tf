35c2d2c4602b80b20da27ee5c8a03c7893d97b40
hitoper <hit.sitapara@operisoft.com>
2025-06-24 15:15:42 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -14,15 +14,3 @@ variable "region" {
   type        = string
   default     = "ap-south-1"
 }
-
-variable "aws_access_key" {
-  description = "AWS access key"
-  type        = string
-  sensitive   = true
-}
-
-variable "aws_secret_key" {
-  description = "AWS secret key"  
-  type        = string
-  sensitive   = true
-}
